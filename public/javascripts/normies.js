728223ecd910b70744a00c9cdc5c1f08df8ffd89
SuperKK <tcr@thecardroom.io>
2022-06-16 21:22:45 -0700

    Add ADApes, Dead Pop Hell, and 666 University

--- public/javascripts/normies.js
+++ public/javascripts/normies.js
@@ -1,5 +1,12 @@
 module.exports = {
     whitelist: {
+        "6710d32c862a616ba81ef00294e60fe56969949e0225452c48b5f0ed": "666 University",
+        "6202c72d0246587cee46bc56cb8722c17556da43307afcdc119fc593": "ADApes #001",
+        "fa89210feed7cb4fbab4ae352c713034cc898b1ebc53db23af04d306": "ADApes Special",
+        "0a14128cae695768a61cbecf45f87750893fa65a3430be2bfdbdc60b": "ADApes Chedda",
+        "a72217d53e81e7075c0198315f57a84825a3a2d62d74ce34baa7b0bb": "ADApes S1",
+        "61326897a69a96ce77f5ddcbccc7f00404b8c10e21ab4bb9cb7e9f96": "ADApes S2",
+        "304cae95a0326705b495da3ddd66826539eead4e693917fdf0860fa5": "ADApes S3",
         "8cea09a81ddf9707fd2c80f544d984bd0e8f16898bba852c53bc1048": "ADAZOO first gen lacie",
         "24382c92f575c4e51c2454824495e5ceb2a08f5c66e48911a2bfd7b2": "ADAZOO land",
         "62593c1dd55795758ee701e72446bba7a33001d219da0af11a1849f5": "ADAZOO poster",
@@ -8,6 +15,7 @@ module.exports = {
         "aa19d5f5ae9b6c93c8e278851194553ddd4789d77f86d3ad2f7480d8": "Cardano Crocs Club",
         "42e6416b9208aa03898a19ac94124282c041f1c7bf08c3e3f3006c06": "Cardano Rock",
         "40fa2aa67258b4ce7b5782f74831d46a84c59a0ff0c28262fab21728": "Clay Nation",
+        "106e44f8ce68e28500545dcb68b407331a9d4b6088f11f672c6d07db": "Dead Pop Hell - Am I Evil",
         "1ec85dcee27f2d90ec1f9a1e4ce74a667dc9be8b184463223f9c9601": "DeadPXLZ",
         "c76e5286fce9e6f5c9b1c5a61f74bc7fa89ed0f946ff2ae5d875f2cb": "El Matador",
         "c3e8e1f5c3e4e5bc1c1802a1e1a2f3e511b7f41e4a71beedf2812062": "Faces",
