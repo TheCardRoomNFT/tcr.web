b4c3bb9a150d6113a3e2a9eaf9b2740db3029fed
SuperKK <103620148+TheCardRoomNFT@users.noreply.github.com>
2023-01-28 14:58:25 +1100

    Integrate payments with attached wallet (#22)
    Co-authored-by: SuperKK <tcr@thecardroom.io>

--- public/javascripts/recaptcha.js
+++ public/javascripts/recaptcha.js
@@ -1,17 +1,62 @@
 
-function show_result(response) {
+const TIMEOUT_ERROR = Symbol();
+const timeout = (prom, seconds) => {
+	let timer;
+	return Promise.race([
+		prom,
+		new Promise((_r, rej) => timer = setTimeout(rej, seconds*1000, TIMEOUT_ERROR))
+	]).finally(() => clearTimeout(timer));
+}
+
+async function show_result(response) {
     success_para = document.getElementById('success-response-message');
     error_para = document.getElementById('error-response-message');
+    var progress = document.getElementById('request-progress')
 
     if (response.success) {
-        success_para.innerHTML = 'Request Received.  Please send 12 ADA to: ' + response.address;
-        success_para.style.display = 'block';
-        error_para.style.display = 'none';
+        try {
+            const txid = await timeout(send_payment(response.address, 12000000), 300);
+            if (txid === null) {
+                success_para.innerHTML = 'Request Received, payment failed.  Please send 12 ADA to: ' + response.address + ' to complete the request.';
+                success_para.style.display = 'block';
+                error_para.style.display = 'none';
+
+                progress.ariaValueNow = 100;
+                progress.style.width = '100%';
+                progress.classList.add('bg-warning');
+                progress.classList.remove('bg-success');
+            } else {
+                success_para.innerHTML = 'Success!  Your mutant will arrive within 1 to 3 days.  TXID = ' + txid;
+                success_para.style.display = 'block';
+                error_para.style.display = 'none';
+
+                progress.ariaValueNow = 100;
+                progress.style.width = '100%';
+            }
+        } catch (e) {
+            if (e === TIMEOUT_ERROR) {
+                success_para.innerHTML = 'Request Received, payment timeout.  Please send 12 ADA to: ' + response.address + ' to complete the request.';
+                success_para.style.display = 'block';
+                error_para.style.display = 'none';
+
+                progress.ariaValueNow = 100;
+                progress.style.width = '100%';
+                progress.classList.add('bg-warning');
+                progress.classList.remove('bg-success');
+            }
+        }
     } else {
         error_para.innerHTML = 'Error: ' + response.error;
         success_para.style.display = 'none';
         error_para.style.display = 'block';
+
+        progress.ariaValueNow = 100;
+        progress.style.width = '100%';
+        progress.classList.add('bg-danger');
+        progress.classList.remove('bg-success');
     }
+
+    document.getElementById('submit-button').disabled = false;
 
     document.getElementById('normie-select').value = '';
     document.getElementById('mutation-select').value = '';
@@ -36,17 +81,35 @@ function handle_click() {
         const wallet = document.getElementById('button-wallet-connect').innerText
         const token = document.getElementById('grecaptcha-token').value;
 
+        document.getElementById('submit-button').disabled = true;
+
+        var progress = document.getElementById('request-progress')
+        var progress_parent = document.getElementById('request-progress-parent')
+
+        progress.classList.remove('bg-danger');
+        progress.classList.remove('bg-warning');
+        progress.classList.add('bg-success');
+
+        progress_parent.hidden = false;
+        progress.ariaValueNow = 0;
+        progress.style.width = '0%';
+
+        var success_para = document.getElementById('success-response-message');
+        var error_para = document.getElementById('error-response-message');
+        error_para.innerHTML = '';
+        success_para.innerHTML = '';
+
         if (!accept || accept.checked === false) {
-            error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Please accept Terms & Conditions';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
 
         if (!wallet || wallet === 'Connect') {
-            error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Error: Wallet not connected';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
 
@@ -54,29 +117,33 @@ function handle_click() {
             error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Error: Invalid normie';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
 
         if (!mutation || !mutation.startsWith('asset')) {
-            error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Error: Invalid mutation';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
 
         if (!algorithm || (algorithm !== 'VQGAN+CLIP' && algorithm !== 'StableDiffusion')) {
-            error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Error: Invalid algorithm';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
 
         if (!from || !from.startsWith('addr')) {
-            error_para = document.getElementById('error-response-message');
             error_para.innerHTML = 'Error: Invalid address';
             error_para.style.display = 'block';
+            document.getElementById('submit-button').disabled = false;
             return;
         }
+
+        progress.ariaValueNow = 5;
+        progress.style.width = '5%';
 
         var data = {
             normie_asset_id: normie,
@@ -98,7 +165,9 @@ function handle_click() {
         }).then(response => {
             return response.json();
         }).then(data => {
-            return show_result(data)
+            return show_result(data);
+        }).then(result => {
+            console.log('complete');
         }).catch(error => {
             return show_result({success: false, error: error});
         });
